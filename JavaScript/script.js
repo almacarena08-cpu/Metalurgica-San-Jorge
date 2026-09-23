0d2f369ce9817f44c764243986d410a77b6a54b0
sanama-lab <omori14102008@gmail.com>
2026-09-23 20:47:15 -0300

    lo tuyo y lo mio

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -24,3 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+const menuToggle = document.getElementById('menuToggle');
+        const sidebar = document.getElementById('sidebar');
+        const sidebarOverlay = document.getElementById('sidebarOverlay');
+        const closeSidebar = document.getElementById('closeSidebar');
+
+        function toggleMenu() {
+            sidebar.classList.toggle('active');
+            sidebarOverlay.classList.toggle('active');
+        }
+
+        menuToggle.addEventListener('click', toggleMenu);
+        closeSidebar.addEventListener('click', toggleMenu);
+        sidebarOverlay.addEventListener('click', toggleMenu);
